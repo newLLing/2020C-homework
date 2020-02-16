e88b07bde68a592ce07b00733c5e308ae0634c4a
Zoseven <1341018016@qq.com>
2020-02-16 14:10:17 +0800

    201910414314-李其林

---
+++ 基础班作业/201910414314-李其林/菱形.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+	int n, b, a = 0;
+	printf("input a jishu£º");
+	scanf_s("%d", &n);
+	b = n;
+	for (int i = 0; i < n - n / 2; i++)
+	{
+		a++; b--;
+		for (int j = i; j < n / 2; j++)
+		{
+			printf(" ");
+		}
+		for (int m = 0; m < 2 * a - 1; m++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	for (int i = 0; i < n - a; i++)
+	{
+		for (int j = 0; j <= i; j++)
+			printf(" ");
+		for (int m = 0; m < 2 * b - 1; m++)
+			printf("*");
+		printf("\n");
+		b--;
+	}
+	return 0;
+}

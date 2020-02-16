c0a0437f4569cda49da8052535eb22b71ce208ba
hechuanhan <61086254+hechuanhan@users.noreply.github.com>
2020-02-16 18:51:05 +0800

    201810412115-何钏涵

---
+++ 基础班作业/201810412115何钏涵/第一题.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int a, b, c;
+	scanf("%d", &a);
+	scanf("%d", &b);
+	c = a + b;
+	printf("%d,%d,%d", a, b, c);
+	system("pause");
+	return 0;
+}
+

6b64e61cb8f1b8e119550d0e4b446e7c7afc74c3
Kersey1323 <492700414@qq.com>
2020-02-18 12:06:04 +0800

    homework

---
+++ 提高班作业/201910414112-况熙/1.1二分查找.cpp
@@ -0,0 +1,38 @@
+﻿#include<stdio.h>
+#define N 100
+int main()
+{
+	int n;
+	int num[N];int aim; int low,mid,high;
+	printf("请输入所输入的数字个数:");
+	scanf("%d", &n);
+	printf("请输入一行数字:");
+	for (int i = 0; i < n; i++)
+		scanf("%d", &num[i]);
+	high = n - 1;
+	low = 0;
+	printf("请输入要查找的数字:");
+	scanf("%d", &aim);
+	mid= (low + high) / 2; 
+	while (low <= high)
+	{
+		mid = (low + high) / 2;
+		if (aim == num[mid])
+			break;
+		else if (aim > num[mid])
+
+			low = mid + 1;
+		else if (aim < num[mid])
+
+			high = mid - 1;
+	}	
+		if (low > high)
+			printf("no");
+		else
+			printf("yes");
+
+	return 0;
+	
+
+
+}
---
+++ 提高班作业/201910414112-况熙/1.2装箱问题.cpp
@@ -0,0 +1,31 @@
+﻿#include<stdio.h>
+
+int v;//箱子体积
+int n;//物品个数
+int a[31];//存放物品的体积
+int dp[10001] = { 0 };//dp[i]表示箱子容积为i时可放入的物品的体积
+
+int main()
+{
+	int i, j, t;
+	printf("请输入箱子的体积:");
+	scanf("%d", &v);
+	printf("请输入物品的个数:");
+	scanf("%d", &n);
+	printf("请输入物品的体积:");
+	for (i = 0; i < n; i++)
+		scanf("%d", &a[i]);
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = v; j >= a[i]; j--)
+		{
+			if (dp[j] > (dp[j - a[i]] + a[i]))
+				dp[j] = dp[j];
+			else
+				dp[j] = dp[j - a[i]] + a[i];
+		}
+	}
+	printf("%d", v - dp[v]);
+	return 0;
+}

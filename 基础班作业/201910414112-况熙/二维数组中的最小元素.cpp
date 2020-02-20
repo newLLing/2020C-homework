d3c6d2e20e425bfae9039fe91329b227669b7454
Kersey1323 <492700414@qq.com>
2020-02-20 14:14:13 +0800

    homework2

---
+++ 基础班作业/201910414112-况熙/二维数组中的最小元素.cpp
@@ -0,0 +1,40 @@
+﻿#include <stdio.h>
+#define M 5
+#define N 2
+void fun(int **);
+
+int main()
+{
+	int i;
+	int tt[M][N] = { {13,2},{3,4},{5,6},{7,8},{9,10} }; 
+	int* p[M];
+	for (i = 0; i < M; i++)
+		p[i] = tt[i];
+	fun(p);
+	return 0;
+}
+
+void fun(int **p)
+{
+	int i;
+	int a[M];
+	for ( i = 0; i < M; i++)
+	{
+		int* q = p[i];
+		int max = *q;
+		a[i] = max;
+		for(int j=0;j<N;j++)
+		{
+			if (*q > max)
+			{
+				max = *q;
+				a[i] = max;
+			}
+			else
+				q++;
+		}
+	}
+	for (i = 0; i < M; i++)
+		printf("%d,", a[i]);
+
+}
---
+++ 基础班作业/201910414112-况熙/小排头 大排尾.cpp
@@ -0,0 +1,38 @@
+﻿#include<stdio.h>
+
+int main()
+{
+
+	int a[10]; int i;
+	printf("请输入10个数字:");
+	for (i = 0; i < 10; i++)
+		scanf_s("%d,", &a[i]);
+	int* p = a;
+
+	int tmp;
+	for (i = 0; i < 10; i++)
+	{
+		if (*p > a[9])
+		{
+			tmp = a[9];
+			a[9] = *p;
+			*p = tmp;
+
+		}
+		p++;
+	}
+	p = a;
+	for (i = 0; i < 10; i++)
+	{
+		if (*p < a[0])
+		{
+			tmp = a[0];
+			a[0] = *p;
+			*p = tmp;
+		}
+		p++;
+	}
+	for (i = 0; i < 10; i++)
+		printf("%d ", a[i]);
+
+}

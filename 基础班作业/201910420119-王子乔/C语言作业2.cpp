b3db17fec0c4ea124e2447f0cb5678b51b6e30f4
Z1Qiao <61087632+Z1Qiao@users.noreply.github.com>
2020-02-20 17:01:25 +0800

    Add files via upload

--- 基础班作业/201910420119-王子乔/C语言作业2.cpp
+++ 基础班作业/201910420119-王子乔/C语言作业2.cpp
@@ -1,17 +1,27 @@
 #include<stdio.h>
+#define N 5
 int main()
 {
-	printf("      *\n");
-	printf("     ***\n");
-	printf("    *****\n");
-	printf("   *******\n");
-	printf("  *********\n");
-	printf(" ***********\n");
-	printf("*************\n");
-	printf(" ***********\n");
-	printf("  *********\n");
-	printf("   *******\n"); 
-	printf("    *****\n");
-	printf("     ***\n");
-	printf("      *");
-} 
+int i,temp;
+int a[5],b[5];
+printf("输入五个数字作为第一个数组:\n");
+for(i=0;i<=4;i++)
+scanf("%d",&a[i]);
+printf("输入五个数字作为第二个数组:\n");
+for(i=0;i<=4;i++)
+scanf("%d",&b[i]);
+for(i=0;i<=4;i++)
+{
+temp=a[i];
+a[i]=b[i];
+b[i]=temp;
+}
+printf("交换元素之后的第一个数组是:\n");
+for(i=0;i<=4;i++)
+printf("%d ",a[i]);
+printf("\n");
+printf("交换元素之后的第二个数组是:\n");
+for(i=0;i<=4;i++)
+printf("%d ",b[i]);
+printf("\n");
+}

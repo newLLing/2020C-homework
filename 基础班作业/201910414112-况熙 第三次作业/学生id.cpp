d55c4491d69c4aabbade8f63e69078180afbe4f9
Kersey1323 <492700414@qq.com>
2020-02-22 13:27:08 +0800

    homework3

---
+++ 基础班作业/201910414112-况熙 第三次作业/学生id.cpp
@@ -0,0 +1,22 @@
+﻿#include<stdio.h>
+
+typedef struct Student {
+	char name[10];
+	char stu_id[15];
+	int age;
+	char sex[5];
+}student;
+student* find_id(char id[], student stus[], int num);
+
+student* find_id(char id[], student stus[], int num)
+{
+	int i;
+	for (i = 0; i < num; i++)
+	{
+		if (id == stus[i].stu_id)
+		{
+			return &stus[i];
+		}
+	}
+		return NULL;
+}

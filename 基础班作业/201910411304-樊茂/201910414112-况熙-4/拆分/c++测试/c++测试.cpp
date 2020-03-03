a2b0bc4de78643dbd365e02ec5ffd7c966fc92e1
Kersey1323 <492700414@qq.com>
2020-03-03 15:45:07 +0800

    homework4

---
+++ 基础班作业/201910411304-樊茂/201910414112-况熙-4/拆分/c++测试/c++测试.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+using namespace std;
+
+
+int main()
+{
+	int i, k, num, sum = 0;
+	cout << "请输入整数:";
+	cin >> num;
+	for (i = 1; i <= num / 2; i++)
+	{
+		for (k = 1;; k++)
+		{
+			sum = (k = 1) * (2 * i + k) / 2;
+			if (sum > num)
+				break;
+			if (num == sum)
+			{
+				for (int j = i; j < i + k; j++)
+				{
+					cout << j << "+";
+				}
+				cout << i + k << "=" << endl;
+			}
+		}
+	}
+	return 0;
+}
+
+
+
+
